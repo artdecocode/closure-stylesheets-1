df71b41b7f7b223900522c0f4ce9a9ce6b5be15a
Anton <anton@adc.sh>
2019-10-08 03:25:20 +0300

    fix allow duplicate detection

--- src/com/google/common/css/compiler/passes/PassRunner.java
+++ src/com/google/common/css/compiler/passes/PassRunner.java
@@ -145,7 +145,7 @@ public class PassRunner {
           cssTree.getMutatingVisitController()).runPass();
     }
     // Report errors for duplicate declarations
-    if (job.allowDuplicateDeclarations) { 
+    if (!job.allowDuplicateDeclarations) {
       new DisallowDuplicateDeclarations(
           cssTree.getVisitController(), errorManager).runPass();
     }
